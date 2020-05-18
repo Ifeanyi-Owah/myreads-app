55405fe6c0cb8ecfa1bdb585f309d7f26da2d50c
Ifeanyi-Owah <ifeanyi.owah.io@gmail.com>
2020-05-18 01:32:35 -0400

    feat:create book and bookshelf component

---
+++ src/BookShelf.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Book from "./Book";
+
+class BookShelf extends Component {
+  render() {
+    return (
+      <div>
+        <Book />
+      </div>
+    );
+  }
+}
+
+export default BookShelf;
